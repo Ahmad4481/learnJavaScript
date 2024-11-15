947c158687d972a2297de8cccdbf39a975b4d4d2
Ahmad4481 <ahmdalfhd222@gmail.com>
2024-11-15 23:10:30 +0300

    end local storage assign 111-114

---
+++ 111-114/assign.js
@@ -0,0 +1,29 @@
+let selects = document.querySelectorAll("div label+select");
+if (!localStorage.color && !localStorage.font && !localStorage.size) {
+  localStorage.setItem("color", color.value);
+  localStorage.setItem("font", font.value);
+  localStorage.setItem("size", size.value);
+}
+selects.forEach((el) => {
+  document.body.style.cssText = `color: ${localStorage.color}; font-family: ${localStorage.font}; font-size: ${localStorage.size};`;
+  el.value = localStorage.getItem(el.id);
+  el.addEventListener("input", function () {
+    localStorage.setItem(this.id, this.value);
+    document.body.style.cssText = `color: ${localStorage.color}; font-family: ${localStorage.font}; font-size: ${localStorage.size};`;
+  });
+});
+
+[one, two, three, select].forEach((el) => {
+  el.value = sessionStorage[el.getAttribute("storge")] ?? "";
+  el.addEventListener("input", function () {
+    sessionStorage.setItem(el.getAttribute("storge"), this.value);
+  });
+});
+
+document.querySelector("#submit").addEventListener("click", function (event) {
+  [one, two, three, select].forEach((el) => {
+    sessionStorage.removeItem(el.getAttribute("storge"));
+  });
+  document.querySelector("form div").remove();
+  event.preventDefault();
+});
