f52dd045a008349ef92ebae862eff8fdfe606e0f
Ahmad4481 <153111702+Ahmad4481@users.noreply.github.com>
2024-11-20 21:22:29 +0300

    Create chalng.js

---
+++ 111-114/chalng.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <style>
+    .task {
+      display: flex;
+      justify-content: space-between;
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <form action="">
+      <input type="text" class="input">
+      <input type="submit" class="add" value="Add Task">
+    </form>
+    <div class="tasks"></div>
+  </div>
+  <script src="chalng.js"></script>
+</body>
+</html>
